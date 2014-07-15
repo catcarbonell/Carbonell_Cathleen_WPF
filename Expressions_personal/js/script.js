0ac1c7bb54050d9bd780dd571f2a7e22ca60dbca
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 15:22:32 -0700

    Expressions Personal - Knives Variable
    Added a prompt to ask for how many knives

--- Expressions_personal/js/script.js
+++ Expressions_personal/js/script.js
@@ -1,8 +1,10 @@
 // Cathleen Carbonell | 7/17/2014 | Expressions Personal
 
-// How many individual training knives should my Eskrima studio order if x amount of students order x amount of knives for the next x months?
+// How many individual training knives should my Eskrima studio order if x amount of students order x amount of knives and how much will it cost total?
 
-//Student variable prompt
-var students = prompt("How many students?");
+// Student variable prompt
+var students = prompt("How many students want to purchase a knife?");
 
+// How many knives ordered? (Let's just say we're grouping the orders by how much they ordered individually)
+var knives = prompt("How many knives did they order, each?");
 
