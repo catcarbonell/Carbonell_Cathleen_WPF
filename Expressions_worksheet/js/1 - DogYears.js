ec3e81c382befd4252a832baf7e06ad8f67cb7be
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-14 22:35:53 -0700

    EW - Dog Years
    Provided formula to convert to dog years

--- Expressions_worksheet/js/1 - DogYears.js
+++
@@ -1,6 +0,0 @@
-//Cathleen Carbonell | 7/17/2014 | Expressions Worksheet
-
-//Calculate how old Sparky the pit bull is in dog years based on his actual age.
-
-//Sparky's human age
-var sparkyHuman = 1;
---
+++ Expressions_worksheet/js/1DogYears.js
@@ -0,0 +1,12 @@
+//Cathleen Carbonell | 7/17/2014 | Expressions Worksheet - Dog Years
+
+//Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+//Sparky's human age
+var sparkyHuman = 1;
+
+//formula for Sparky's age in dog years
+var sparkyDog =  sparkyHuman * 7;
+
+//result to print
+console.log(sparkyDog)
