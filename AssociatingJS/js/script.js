97dc0d039fc434d5df4db710ba6734873b483c6b
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-14 21:17:20 -0700

    Expressions 36 - Casting 5
    Added together as characters

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -4,6 +4,6 @@ var areaCode = 510;
 var firstPart = 851;
 var secPart = 1575;
 
-var phoneNo = areaCode + firstPart + secPart;
+var phoneNo = String(areaCode) + String(firstPart) + String(secPart);
 
 console.log(phoneNo);
