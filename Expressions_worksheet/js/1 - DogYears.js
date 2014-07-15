eb038722ee76125585fde38a70770bb3d1f593d1
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-14 22:17:09 -0700

    EW - Dog Years - Human Age
    Sparky’s human age variable

--- Expressions_worksheet/js/1 - DogYears.js
+++ Expressions_worksheet/js/1 - DogYears.js
@@ -1,1 +1,6 @@
-alert("This is in the js file");
+//Cathleen Carbonell | 7/17/2014 | Expressions Worksheet
+
+//Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+//Sparky's human age
+var sparkyHuman = 1;
