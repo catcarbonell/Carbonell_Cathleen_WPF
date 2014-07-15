a89480dd4484876a2ff5f37c4a149114721410e7
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-14 21:18:18 -0700

    Expressions 37 - Casting 6
    Formatted phone number

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -4,6 +4,6 @@ var areaCode = 510;
 var firstPart = 851;
 var secPart = 1575;
 
-var phoneNo = String(areaCode) + String(firstPart) + String(secPart);
+var phoneNo = "(" + String(areaCode) + ")"  + String(firstPart) + "-" + String(secPart);
 
 console.log(phoneNo);
