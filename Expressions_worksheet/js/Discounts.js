a80597a51d5a45daa1c1c922151fee32ac99319e
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 09:05:57 -0700

    EW - Discounts - Total taken
    Found what 25% from $20 is

--- Expressions_worksheet/js/Discounts.js
+++ Expressions_worksheet/js/Discounts.js
@@ -6,8 +6,11 @@
 var scarf = 20;
 
 // Discount = 25% = 0.25
-var discount = 0.25;
+var discperc= 0.25;
 
 // Tax = 7.5% = 0.075
 var tax = 0.075;
 
+// Discount equation part 1: finding how much is exactly taken off the original price = Original price * discount
+var discount = scarf * discperc;
+console.log(discount);
