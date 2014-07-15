193953cf12b7eedc11ab75b883544d582e482b37
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 08:45:23 -0700

    EW - Shopping Bill - Given
    Provided the groceries array

--- Expressions_worksheet/js/Shopping.js
+++ Expressions_worksheet/js/Shopping.js
@@ -1,1 +1,6 @@
-alert("This is in the js file");
+// Cathleen Carbonell | 7/17/2014 | Shopping Bill
+
+//Only one given required
+
+//Array of groceries spent in the last five weeks
+var groceries = [36, 48, 25, 30, 28]
