68988a1741169caeb73b5c4a4cde78f8677f5364
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-14 21:19:35 -0700

    Expressions 38 - Assignment Operators
    Wiped file for next video

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,9 +1,1 @@
-//Expressions - Casting
-
-var areaCode = 510;
-var firstPart = 851;
-var secPart = 1575;
-
-var phoneNo = "(" + String(areaCode) + ")"  + String(firstPart) + "-" + String(secPart);
-
-console.log(phoneNo);
+//Expressions - Assignment Operators
