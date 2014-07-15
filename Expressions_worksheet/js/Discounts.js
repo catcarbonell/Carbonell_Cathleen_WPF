7f3221ab21258c489cab88b7ecf7864c1dee0191
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 09:00:17 -0700

    EW - Discounts - Givens
    Provided givens of item and original price, discount, and tax
    percentages

--- Expressions_worksheet/js/Discounts.js
+++ Expressions_worksheet/js/Discounts.js
@@ -1,1 +1,13 @@
-alert("This is in the js file");
+// Cathleen Carbonell | 7/15/2014 | Discounts
+
+// Givens needed: Original price, discount percentage, description of item, sales tax
+
+// Original price and item
+var scarf = 20;
+
+// Discount = 25% = 0.25
+var discount = 0.25;
+
+// Tax = 7.5% = 0.075
+var tax = 0.075;
+
