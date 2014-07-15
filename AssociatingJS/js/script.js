69a406d4b9a6ec33f691669994c9a9fb9660d58b
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-14 20:36:31 -0700

    Expressions 17 - PEMDAS 2
    Provided data

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,8 @@
 //Expressions - PEMDAS - The Order of Operations
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items, divided by the number of items
