56e6afa73b8b159bd4588b1378a43e47590f02ce
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 16:07:13 -0700

    Expressions Industry - Resolution Width
    Provided a variable for the resolution width

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -2,8 +2,16 @@
 
 // Find the percentage of how much a fixed-size container in a layout will take up in a certain resolution screen or browser.
 
-//First, we will need to find the height and width of the fixed-size container
+// First, we will need to find the width of the fixed-size container
 var containerwidth = prompt("What is the pixel width of your container?");
+
+// Next, we will need the height
 var containerheight = prompt("What is its height?");
 
+// Of course, we will need the area of both:
 var container = containerwidth * containerheight;
+
+// Next, we should find out the resolution of their viewing method (browser, screen-- doesn't matter. We just need the resolution of it)
+var reswidth = prompt("What is your resolution width?");
+
+
