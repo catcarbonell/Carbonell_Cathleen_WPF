4be8419bb0097726cab2580e1f17354f423950ae
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 15:59:31 -0700

    Expressions Industry - Container info
    Provided variables for container width x height plus equation

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -1,2 +1,9 @@
 // Cathleen Carbonell | 7/17/2014 | Expressions Industry
 
+// Find the percentage of how much a fixed-size container in a layout will take up in a certain resolution screen or browser.
+
+//First, we will need to find the height and width of the fixed-size container
+var containerwidth = prompt("What is the pixel width of your container?");
+var containerheight = prompt("What is its height?");
+
+var container = containerwidth * containerheight;
