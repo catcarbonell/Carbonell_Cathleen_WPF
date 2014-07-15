f2581b6ad40f8191a630d3df20b2e523de8bc960
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 15:04:43 -0700

    Expressions Personal - Student Variable
    Provided student variable prompt

--- Expressions_personal/js/script.js
+++ Expressions_personal/js/script.js
@@ -1,2 +1,8 @@
 // Cathleen Carbonell | 7/17/2014 | Expressions Personal
 
+// How many individual training knives should my Eskrima studio order if x amount of students order x amount of knives for the next x months?
+
+//Student variable prompt
+var students = prompt("How many students?");
+
+
