f4f58a39dfdbcc859b088f9b4dc772af24384607
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 08:50:12 -0700

    EW - Shopping Bill - Total Groceries
    Total sum of groceries

--- Expressions_worksheet/js/Shopping.js
+++ Expressions_worksheet/js/Shopping.js
@@ -3,4 +3,8 @@
 //Only one given required
 
 //Array of groceries spent in the last five weeks
-var groceries = [36, 48, 25, 30, 28]
+var groceries = [36, 48, 25, 30, 28];
+
+//Total spent on groceries
+var totalgroc = groceries[0]+ groceries[1] + groceries[2] + groceries[3] + groceries[4];
+console.log(totalgroc);
