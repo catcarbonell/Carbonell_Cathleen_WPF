2f8b8c46a8de8b21408a626c1e41c2d7feeff199
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 16:57:39 -0700

    Expressions Wacky - Setting the stage
    Commented on how the word problem syntax can be arranged

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,5 @@
-alert("This is in the js file");
+// Cathleen Carbonell | 7/17/204 | Expressions Wacky
+
+// We are to find how much room 3 plush pigs take up in sq ft.
+
+// Let's get the sizes of the plush pigs in inches (because plush toys are usually not measured in sq ft)
