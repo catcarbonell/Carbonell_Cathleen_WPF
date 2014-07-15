144de9294ff9420a1da77b4bb37e7dd989c4e46e
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 07:50:41 -0700

    EW - Slice of Pie Part 1 - Givens
    Provided the given information

--- Expressions_worksheet/js/SliceofPie.js
+++ Expressions_worksheet/js/SliceofPie.js
@@ -1,2 +1,13 @@
 // Cathleen Carbonell | 7/17/2014 | Expressions Worksheet -- Slice of Pie Part 1
 
+// 20 people, 8 slices per pizza, 4 pizzas ordered
+
+// Slices variable
+var slices = 8;
+
+// People variable
+
+var people = 20;
+
+// Pizzas variable
+var pizza = 4;
