2530c294b664e375526423c298d1f8a56eff12d5
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 17:05:56 -0700

    Expressions Wacky - Piggies
    Created the prompts and the array for pig plush info

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,3 +3,9 @@
 // We are to find how much room 3 plush pigs take up in sq ft.
 
 // Let's get the sizes of the plush pigs in inches (because plush toys are usually not measured in sq ft)
+
+var pig1 = prompt("What's the size of pig 1 in inches?");
+var pig2 = prompt("What is the size of pig 2?");
+var pig3 = prompt("Finally, pig 3?");
+
+var piggies = [pig1, pig2, pig3];
