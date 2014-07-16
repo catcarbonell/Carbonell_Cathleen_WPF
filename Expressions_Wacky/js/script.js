78e702fa0919ee229c2e28e50d8fcbd35c9b2194
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-15 17:37:21 -0700

    Expressions Wacky - Piggies Array
    Re-did the code to write an array

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,10 +3,11 @@
 // We are to find how much room 3 plush pigs take up in sq ft.
 
 // Let's get the sizes of the plush pigs in inches (because plush toys are usually not measured in sq ft)
+// Variable automatically converts the measurement into sq ft
 
-var pig1 = prompt("What's the size of pig 1 in inches?") / 12;
-var pig2 = prompt("What is the size of pig 2?") / 12;
-var pig3 = prompt("Finally, pig 3?") /12;
+var pig1 = prompt("What's the size of pig 1 in inches?");
+var pig2 = prompt("What is the size of pig 2?");
+var pig3 = prompt("Finally, pig 3?");
 
+//We need to find the sq ft measurement of the pigs, so since there are 12 inches to 1 sq ft, we divide the number by 12
 var piggies = [pig1, pig2, pig3];
-
