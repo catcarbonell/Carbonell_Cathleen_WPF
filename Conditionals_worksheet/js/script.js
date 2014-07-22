74acf7e950668c4b46c2fd955ef0a72283652d7e
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-22 16:31:59 -0700

    CW -- Gas -- Givens
    Provided given variables

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,1 +1,8 @@
 // Cathleen Carbonell 7/24/2014 Conditionals Worksheet
+
+// Last Chance for Gas!
+// Given: Gas efficiency of car in mpg, Gauge reading of gas tank in %, Car's gas tank capacity in gallons
+
+var efficiency = 10;
+var tankremain = 7;
+var capacity = 15;
