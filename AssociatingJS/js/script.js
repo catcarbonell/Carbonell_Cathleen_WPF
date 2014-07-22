8a95a55afc54d1ec1f714c06d445ff14e78386bb
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-22 15:09:21 -0700

    Conditional Logic -- Logical Operators
    budget/iphone scenario

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,19 +1,11 @@
-// Conditional Logic -- With an Expression
+// Conditional Logic -- Logical Operators
 
-var kidHeight = 48;
-var minHeight = 48;
-var wParentHeight = 45; // height of the kid with parent
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
 
-//if the child is old enough, print to the console, "you can ride!"
-//if the kid is 48 in in height
-
-if(kidHeight > minHeight){
-    //you can ride!
-    console.log("You can ride the roller coaster!");
-}else if(kidHeight > wParentHeight){
-   //you can ride with a parent present
-    console.log("You can ride, but only with a parent present.");
+if(iPhonePrice < budget){
+    console.log("We can buy the phone!");
 }else{
-  //sorry you have growing to do
-    console.log("Sorry, grow faster!");
+    console.log("No phone for you!")
 }
