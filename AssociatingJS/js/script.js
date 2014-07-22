3ec78eb2e703a7fb684b8ee99fe0d386106ee697
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-22 13:18:40 -0700

    Conditional Logic — Else If 2
    Added kid w/ parent variable

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -2,6 +2,7 @@
 
 var kidHeight = 48;
 var minHeight = 48;
+var wParentHeight = 45; // height of the kid with parent
 
 //if the child is old enough, print to the console, "you can ride!"
 //if the kid is 48 in in height
@@ -9,7 +10,7 @@ var minHeight = 48;
 if(kidHeight > minHeight){
     //you can ride!
     console.log("You can ride the roller coaster!");
-}else if( ){
+}else if(kidHeight > wParentHeight){
    //you can ride with a parent present
     console.log("You can ride, but only with a parent present.");
 }else{
