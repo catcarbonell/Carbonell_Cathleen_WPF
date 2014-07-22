4d7ef238da4cd8cfbebf15f557df351fb9be855f
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-22 16:42:39 -0700

    CW -- Gas -- Remainder
    Created var for the result of the calculation of the givens

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -3,13 +3,14 @@
 // Last Chance for Gas!
 // Given: Gas efficiency of car in mpg, Gauge reading of gas tank in %, Car's gas tank capacity in gallons
 
-var efficiency = prompt("Enter your car's gas efficiency in MPG.")
+var efficiency = prompt("Enter your car's gas efficiency in MPG.");
 var tankremain = prompt("Enter how much gas is left in your tank in %.");
 var capacity = prompt("Enter the car's gas capacity in gallons.");
+var remainder = efficiency * tankremain;
 
 //
-if(efficiency * tankremain >= capacity){
+if(remainder >= capacity){
     console.log("Yes, you can make it without stopping for gas!");
-}else{
-    console.log("You only have" +  + "gallons of gas in your tank, better get gas now while you can!");
+}else if(remainder < capacity){
+    console.log("You only have" + remainder + "gallons of gas in your tank, better get gas now while you can!");
 }
