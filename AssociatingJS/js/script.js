8e7d4efc1cb7fe6cb337acd39d4845f50fbf6227
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-22 13:17:22 -0700

    Conditional Logic — Else If
    Else if statements— where conditionals can be used with else

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,16 +1,18 @@
 // Conditional Logic -- With an Expression
 
-var kidHeight = 47;
+var kidHeight = 48;
 var minHeight = 48;
 
 //if the child is old enough, print to the console, "you can ride!"
 //if the kid is 48 in in height
 
 if(kidHeight > minHeight){
-    //code performed if condition is true
+    //you can ride!
     console.log("You can ride the roller coaster!");
-}
-
-if(kidHeight < minHeight){
+}else if( ){
+   //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
+}else{
+  //sorry you have growing to do
     console.log("Sorry, grow faster!");
 }
