ef743392a059e3a60d95abc62a5bc7150df6a7a8
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-22 16:39:58 -0700

    CW -- Gas -- If/Else
    Wrote possible if else statements. Still sorting out what to do.
    Created prompts.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -3,6 +3,13 @@
 // Last Chance for Gas!
 // Given: Gas efficiency of car in mpg, Gauge reading of gas tank in %, Car's gas tank capacity in gallons
 
-var efficiency = 10;
-var tankremain = 7;
-var capacity = 15;
+var efficiency = prompt("Enter your car's gas efficiency in MPG.")
+var tankremain = prompt("Enter how much gas is left in your tank in %.");
+var capacity = prompt("Enter the car's gas capacity in gallons.");
+
+//
+if(efficiency * tankremain >= capacity){
+    console.log("Yes, you can make it without stopping for gas!");
+}else{
+    console.log("You only have" +  + "gallons of gas in your tank, better get gas now while you can!");
+}
