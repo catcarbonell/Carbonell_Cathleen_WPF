2d9c9f690fb06a8371df7465fad7098e6cce65d4
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-22 12:13:55 -0700

    Conditional Logic -- Multiple statements
    if/else

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -6,7 +6,11 @@ var minHeight = 48;
 //if the child is old enough, print to the console, "you can ride!"
 //if the kid is 48 in in height
 
-if(kidHeight + 2 > 48){
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the roller coaster!");
+}
+
+if(kidHeight < minHeight){
+    console.log("Sorry, grow faster!");
 }
