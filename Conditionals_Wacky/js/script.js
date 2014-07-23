8bcaf89cfd5f171be30674f43beff44ccd071fe7
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-23 11:33:55 -0700

    Conditionals -- Wacky -- Validation isNaN

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -14,9 +14,18 @@ var royalrumble = wrestlers/area;
 // We need a percentage equation for print.
 var percentage = royalrumble * 100;
 
+// Code Validation
+if( wrestlers || width || height === ""){
+    console.log = prompt ("HEY. Type in a NUMBER! Don't make me dropkick you!");
+}
+//Testing if the user input is not a number
+if ( isNaN(wrestlers || width || height)){
+    console.log = prompt("HEY. Enter a valid NUMBER! Don't make me dropkick you!");
+}
 // Finally, we have our If/Else If statements:
 if(royalrumble >= 1){
     console.log("Congratulations! You have enough wrestlers to run a Royal Rumble! OOH YEAH!");
 }else if(royalrumble < 1){
     console.log("Sorry, brother. You're only at " + percentage + "%! To run a Royal Rumble in a " + area + " sq ft ring, You'll need to recruit more wrestlers!");
 }
+
