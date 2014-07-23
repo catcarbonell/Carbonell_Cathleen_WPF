18980f6cdf0bb3860eb3f12f30f95d4ac422aaf4
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-23 12:57:00 -0700

    Conditionals -- Personal -- Prompts

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,7 @@
 // Cathleen Carbonell 7/24/2014 Conditionals -- Personal
 
-// Goal Setting! If you want something, you must fight to save for it. But it's best if you know the timeline in which you'll achieve your goal!
+// Goal Setting! If you want something, you must fight to save for it. But it's best if you know the time line in which you'll achieve your goal!
+
+//First, we'll gather information
+var goalcost = prompt("You want to save up for something special! \n How much is it?");
+var savings = prompt("How much do you plan on saving per day?");
