6e851aef2615ebfc79c10a2d11b2239ecb413e99
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-23 14:23:01 -0700

    Conditionals -- Industry -- Image Variables

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,15 @@
-alert("This is in the js file");
+// Cathleen Carbonell | 7/23/2014 | Conditionals -- Industry
+
+// Res Check -- We're trying to find out if an image, measured in pixels, will fit in a certain-sized container.
+
+// Var needed: image w * h in pixels, container w * h in pixels
+
+var imagew = prompt("Enter the image width in pixels.");
+
+
+var imageh = prompt("Enter the image height in pixels.");
+
+
+
+
+
