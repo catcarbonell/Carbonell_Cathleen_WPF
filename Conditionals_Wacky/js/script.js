5895274a1713be771b67f5687d85fc1a493c1c2c
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-23 09:13:30 -0700

    Conditionals -- Wacky -- Comments

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,4 @@
-alert("This is in the js file");
+// Cathleen Carbonell 7/24/2014 Conditionals -- Wacky
+
+// Are you ready for a Royal Rumble??
+// Running one is dependent on the size of the ring and the amount of wrestlers we have 
