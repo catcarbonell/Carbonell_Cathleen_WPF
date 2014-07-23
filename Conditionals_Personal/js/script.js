450d15549bd5c1b9eca202ae153871b97551d87c
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-23 12:49:11 -0700

    Conditionals -- Personal -- Setup

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,3 @@
+// Cathleen Carbonell 7/24/2014 Conditionals -- Personal
+
+// Goal Setting! If you want something, you must fight to save for it. But it's best if you know the timeline in which you'll achieve your goal!
