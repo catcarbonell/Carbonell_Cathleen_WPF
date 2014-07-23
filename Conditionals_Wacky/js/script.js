1597929290596f85312c94cf66287f843359b76c
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-23 10:24:32 -0700

    Conditionals -- Wacky -- Givens
    Provided given prompts for user input

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,4 +1,8 @@
 // Cathleen Carbonell 7/24/2014 Conditionals -- Wacky
 
 // Are you ready for a Royal Rumble??
-// Running one is dependent on the size of the ring and the amount of wrestlers we have 
+// Running an infamous Royal Rumble is dependent on the size of the ring and the amount of wrestlers we have
+
+var wrestlers = prompt("How many wrestlers do we have?");
+var width = prompt("Enter the width of the ring in ft.");
+var height = prompt("Enter the height of the ring in ft.");
