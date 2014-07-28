531e3d12a0932c8e45877170ceb2b15deedd20a1
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-28 13:08:44 -0700

    Functions -- Screencast -- Dog Age
    Testing the dogAge function

--- Functions_Screencast/js/script.js
+++ Functions_Screencast/js/script.js
@@ -1,8 +1,9 @@
 //Functions - Parameters and Arguments
 
-calcArea(30, 20);
-
-function calcArea(w, h){ //w=30, h=20
-    var area = w * h;
-    console.log(area);
+function dogYears(age){
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.")
 }
+
+dogYears(4);
+dogYears(5);
