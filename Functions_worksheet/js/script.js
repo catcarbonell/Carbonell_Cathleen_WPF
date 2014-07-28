0a5fc19910edaefedee0131000966cbacf786c95
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-28 14:20:22 -0700

    Functions -- Worksheet -- Setup

---
+++ Functions_worksheet/js/script.js
@@ -0,0 +1,5 @@
+// Cathleen Carbonell 7/31/2014 Functions Worksheet
+
+// Stung! It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function.
+
+
