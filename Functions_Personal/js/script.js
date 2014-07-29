b404c38f33e37813e6ae8c2415bef648996b564a
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-29 15:54:13 -0700

    Functions -- Personal -- More formatting

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,25 +3,24 @@
 // Let's find out your 1 Rep Max! This is to figure out the maximum weight you can lift!
 
 function repMax(){
+
+    //WEIGHT -- User interaction -- how much weight did the user lift?
+    var weight = prompt("How much weight did you lift in lbs?");
+
+    //  WEIGHT Validation: while loop isNaN and empty string
+    while(isNaN(weight) || weight === "" || weight === 0){
+        weight = prompt("Bro, do you even lift?? \n How much weight did you lift in lbs?");
+    }
+
+    //REPS -- User interaction -- how many reps did they do with that weight?
+    var reps = prompt("How many reps did you do?");
+
+    //REPS Validation: while loop isNaN and empty string
+    while(isNaN(reps) || reps === "" || reps === 0){
+        reps = prompt("Bro, do you even lift?? \n How many reps did you do?");
+    }
     return weight *(1 + reps / 30);
 }
 
-
-//WEIGHT -- User interaction -- how much weight did the user lift?
-var weight = prompt("How much weight did you lift in lbs?");
-
-//  WEIGHT Validation: while loop isNaN and empty string
-while(isNaN(weight) || weight === "" || weight === 0){
-    weight = prompt("Bro, do you even lift?? \n How much weight did you lift in lbs?");
-}
-
-
-//REPS -- User interaction -- how many reps did they do with that weight?
-var reps = prompt("How many reps did you do?");
-
-//REPS Validation: while loop isNaN and empty string
-while(isNaN(reps) || reps === "" || reps === 0){
-    reps = prompt("Bro, do you even lift?? \n How many reps did you do?");
-}
 
 console.log("The most you can lift is " + repMax() + " lbs! Focus!");
