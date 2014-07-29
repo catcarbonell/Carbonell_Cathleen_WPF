e6970d1c7e6cb49639806463e965e70d343b3f24
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-29 02:40:26 -0700

    Functions -- Personal -- Variables
    Placed variables needed

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,6 +2,28 @@
 
 // Let's find out your 1 Rep Max! This is to figure out the maximum weight you can lift!
 
-function repMax(weight, reps){  
+
+
+//WEIGHT -- User interaction -- how much weight did the user lift?
+var weight = prompt("How much weight did you lift in lbs?");
+
+//  WEIGHT Validation: while loop isNaN and empty string
+while(isNaN(weight) || weight === ""){
+    weight = prompt("Bro, do you even lift?? \n How much weight did you lift in lbs?");
+}
+
+//REPS -- User interaction -- how many reps did they do with that weight?
+var reps = prompt("How many reps did you do?");
+
+//REPS Validation: while loop isNaN and empty string
+while(isNaN(reps) || reps === ""){
+    reps = prompt("Bro, do you even lift?? \n How many reps did you do?");
+}
+
+function repMax(weight, reps){
+
     var equation = weight * 1 + reps / 30;
 }
+
+
+console.log(repMax());
