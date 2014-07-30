f635be4ffd1d898f74d9573e3161d41b6b01a4fa
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-30 13:11:16 -0700

    Functions -- Industry -- Calculation adjustment
    Fixed the image function’s calculation.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,29 +5,24 @@
 // We will need the width and height of each image, plus the option to resize the image by a certain percent (or rather, decimal)  as well as how many images are in the slider already, as well as the resolution of the layout.
 
 function images(imgW, imgH, shrink){
-    var imgSize = imgW * imgH / shrink;
+    var imgSize = imgW * imgH * shrink;
     return imgSize;
 }
 
 // USER INTERACTION
 
-// in terms of var, this is what we need:
-/*var imgW = prompt("Enter the width in pixels"); // asking for the image width
-var imgH = prompt("Enter the height in pixels"); // asking for image height
-var shrink = prompt ("In terms of percentages, how much would you like to shrink your image? \n (Enter a number 1-100)") / 100;
-*/
-
 var numImg = prompt ("How many images are there in this gallery?");
 
 // Utilizing the function:
 var gallery = images(prompt("Enter the width in pixels"), prompt("Enter the height in pixels"),
-    prompt ("In terms of percentages, how much would you like to shrink your image? \n (Enter a number 1-100)") / 100);
+    prompt ("How much would you like to adjust your image? \n " +
+            "(Enter a number 1-100, with 100 as keeping it at its full resolution)") / 100);
 
 // We will need to know the area of the entire gallery vs. the screen resolution.
 var galleryArea = gallery * numImg;
 
 //First print out:
-console.log("There are " + numImg + " that take up " + galleryArea + " pixels total.");
+console.log("There are " + numImg + " images that take up " + galleryArea + " pixels total.");
 
 // We also need the layout area
 
@@ -36,6 +31,13 @@ function layout(layoutW,layoutH){
     return resSize;
 }
 
+// The function in action:
 var resArea = layout(prompt("Enter the resolution width in pixels:"), prompt("Enter the resolution width in height:"));
 
+
 console.log("The area of your layout is " + resArea + " pixels." );
+
+// The final verdict
+imgFit = (resArea > galleryArea) ? "Perfect!" : "Consider resizing your images or reducing the amount of images!";
+
+console.log(imgFit);
