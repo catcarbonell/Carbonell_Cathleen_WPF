58214165ac07c1c8b0a4249e70a2bfd05d446c4d
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-30 02:16:24 -0700

    Functions -- Wacky -- Initial Setup
    I thought in terms of order in which the code will be executed. So far
    so good, now to execute a loop until the enemy is legitimately defeated.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,7 +3,7 @@
 /*  ENEMY ENCOUNTERED! -- This is a little numbers-based game. The User has 50 health points (HP)
  and the enemy has 100 HP. The object of the game is to get the enemy hp down to zero before
  you run out of HP.
-
+F
     The attack points (AP) is based on what ever the user enters into the prompt,
  with a hit point limit maximum of 10, and a minimum of 1. It is then multiplied by the
  random number generator (Math.random()).
@@ -12,4 +12,23 @@
  multiplied by 2.
 
     Let's start with what we know... */
+var enemyHP = 100; // Everything will depend on enemyHP.
 
+// Round 1... FIGHT!
+var battleStart = prompt(" !!! ENEMY ENCOUNTERED !!! \n You can only hit him for up to 10 pts per round! \n How much power will you expend?");
+var initialFight = enemyHP - battleStart;
+
+console.log(initialFight);
+
+if(initialFight <= 0){
+    console.log("Congratulations! You have slain your enemy!");
+}
+else{
+   userAtk();
+}
+
+function userAtk(initialFight){
+    var battle = prompt("Oh no! The enemy is still alive! How much power will you expend?");
+    var userAP = initialFight - battle;
+    return userAP;
+}
