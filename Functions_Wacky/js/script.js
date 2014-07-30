dc6a9d1a2b3c10a870da230b4a34265c6f936998
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-29 19:37:40 -0700

    Functions -- Wacky -- Introduction
    I have big plans. Big. Plans.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,2 +1,14 @@
 // Cathleen Carbonell 7/29/2014 Functions - Wacky
 
+/*  ENEMY ENCOUNTERED! -- This is a little numbers-based game. The User has 50 health points (HP)
+ and the enemy has 100 HP. The object of the game is to get the enemy hp down to zero before
+ you run out of HP.
+
+    The attack points (AP) is based on what ever the user enters into the prompt,
+ with a hit point limit maximum of 10, and a minimum of 1. It is then multiplied by the
+ random number generator (Math.random()).
+
+    At the same time, the enemy will be attacking, too, hitting for what the user attacks for,
+ multiplied by 2.
+
+    Let's start with the function for user attack! */
