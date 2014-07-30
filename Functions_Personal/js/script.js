242d8744ffc5fba8ac217dbf3a72dc367a5b1898
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-29 18:07:57 -0700

    Functions -- Personal -- Nevermind
    Didn’t work. Removed.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,7 +2,7 @@
 
 // Let's find out your 1 Rep Max! This is to figure out the maximum weight you can lift!
 
-function repMax(weight, reps){
+function repMax(){
     return weight *(1 + reps / 30);
 }
 
