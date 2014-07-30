dfebdcfdd3923d8fe189c14c70838356286e4174
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-29 23:31:24 -0700

    Functions — Wacky -- Reset
    I may have been in-over-my-head about my initial project. Starting over
    with something much more simpler.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,36 +1,7 @@
 // Cathleen Carbonell 7/29/2014 Functions - Wacky
 
-/*  ENEMY ENCOUNTERED! -- This is a little numbers-based game. The User has 50 health points (HP)
- and the enemy has 100 HP. The object of the game is to get the enemy hp down to zero before
- you run out of HP.
+/*  ENEMY ENCOUNTERED! -- This is the beginnings of a numbers-based game.
 
-    The attack points (AP) is based on what ever the user enters into the prompt,
- with a hit point limit maximum of 10, and a minimum of 1. It is then multiplied by the
- random number generator (Math.random()).
 
-    At the same time, the enemy will be attacking, too, hitting for what the user attacks for,
- multiplied by 2.
 
-    Let's start with what we know... */
-
-//var userHP = 50; // This is what everything will be centered around: the user's and the enemy's health.
-var enemyHP = 100;
-
-var gameStart = prompt("ENEMY ENCOUNTERED!! You must defeat him before he gets you! \n How much attack points (AP) will you spend? \n (minimum: 1 , maximum: 10)");
-var resultStart = enemyHP - gameStart;
-
-var secondStrike = resultStart - userAttack();
-
-var userInteract = prompt('Your enemy is still alive!! \n How much AP will you spend?');
-
-function userAttack(userInteract) {
-    var userAP = userInteract * Math.random();
-    return userAP;
-}
-
-if (strike <= 0) {
-    console.log("Congratulations! You have slain your enemy!!");
-} else if (strike > 0) {
-    userAttack();
-}
 
