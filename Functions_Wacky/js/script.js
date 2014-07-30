1399f7630632a3f06746e78c11c3d0895aee0b8a
Cathleen Carbonell <catcarbonell@gmail.com>
2014-07-30 04:08:06 -0700

    Functions -- Wacky -- Formatting & Comments
    Finally done.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,34 +1,43 @@
-// Cathleen Carbonell 7/29/2014 Functions - Wacky
+// Cathleen Carbonell 7/27/2014 Functions -- Wacky
+// ENEMY ENCOUNTERED! -- This is the beginnings of a numbers-based game. The user must get the enemyHP down to zero.
 
-/*  ENEMY ENCOUNTERED! -- This is a little numbers-based game. The User has 50 health points (HP)
- and the enemy has 100 HP. The object of the game is to get the enemy hp down to zero before
- you run out of HP.
-F
-    The attack points (AP) is based on what ever the user enters into the prompt,
- with a hit point limit maximum of 10, and a minimum of 1. It is then multiplied by the
- random number generator (Math.random()).
+// Initial given: the enemyHP
+var enemyHP = 20;
 
-    At the same time, the enemy will be attacking, too, hitting for what the user attacks for,
- multiplied by 2.
-
-    Let's start with what we know... */
-var enemyHP = 100; // Everything will depend on enemyHP.
-
-// Round 1... FIGHT!
+// Round 1, FIGHT! --  must initially prompt the user for how much attack points they will use
 var battleStart = prompt(" !!! ENEMY ENCOUNTERED !!! \n You can only hit him for up to 10 pts per round! \n How much power will you expend?");
-var initialFight = enemyHP - battleStart;
 
-console.log(initialFight);
+// Since we have a few parameters for the fight, we must validate the prompt:
 
-if(initialFight <= 0){
+    while(isNaN(battleStart) || battleStart === "" || battleStart > 10 || battleStart < 2 ){
+      battleStart = prompt("HEY! Who the hell do you think you are?? \n Type in a number between 1-10!");
+    }
+
+// We will need the initial battle result.
+var battle = enemyHP - battleStart;
+
+// Conditional statement considering how the battle goes
+
+if(battle <= 0){
     console.log("Congratulations! You have slain your enemy!");
 }
 else{
-   userAtk();
+    while(battle > 0){
+        userAtk();
+    }
+    console.log("Congratulations! You have slain the enemy!");
+    window.alert("You Win!");
 }
+// The obligatory function, utilizing the battle result and the user's new variable input
+function userAtk(){
+    var attackPoints = prompt("Oh no! The enemy is still alive! How much power will you expend?");
+    userAP = battle - attackPoints;
+    battle = userAP;
 
-function userAtk(initialFight){
-    var battle = prompt("Oh no! The enemy is still alive! How much power will you expend?");
-    var userAP = initialFight - battle;
-    return userAP;
-}
+    // Validating the attackPoints prompt
+    while(isNaN(attackPoints) || attackPoints > 10 || attackPoints < 2 || attackPoints === ""){
+    attackPoints = prompt("HEY! Who the hell do you think you are?? \n Type in a number between 1-10!")
+
+     }
+
+ }
